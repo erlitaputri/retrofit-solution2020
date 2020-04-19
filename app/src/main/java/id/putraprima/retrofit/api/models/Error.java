df8bec91adac8afdb5c09342a5088865a5274d3f
Erlita Putri Rahmawati <erlitarahmawati8@gmail.com>
2020-04-19 19:24:00 +0700

    session login

--- app/src/main/java/id/putraprima/retrofit/api/models/Error.java
+++ app/src/main/java/id/putraprima/retrofit/api/models/Error.java
@@ -17,4 +17,28 @@ public class Error {
         this.email = email;
     }
 
+    @SerializedName("password")
+    @Expose
+    private List<String> password = null;
+
+    public List<String> getPassword(){
+        return password;
+    }
+
+    public void setPassword(List<String> password){
+        this.password = password;
+    }
+
+    @SerializedName("name")
+    @Expose
+    private List<String> name;
+
+    private List<String> getName(){
+        return name;
+    }
+
+    public void setName(List<String> name){
+        this.name = name;
+    }
+
 }
